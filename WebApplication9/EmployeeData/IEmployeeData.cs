117bf52c845df88c420135efae3b1834c1694120
Admin <Admin@AVINASH>
2022-06-29 16:16:01 +0530

    Add project files.

---
+++ WebApplication9/EmployeeData/IEmployeeData.cs
@@ -0,0 +1,17 @@
+﻿using WebApplication9.Models;
+
+namespace WebApplication9.EmployeeData
+{
+    public interface IEmployeeData
+    {
+        List<Employee> GetEmployees();
+
+        Employee GetEmployee(Guid Id);
+
+        Employee AddEmployee(Employee employee);
+
+        void DeleteEmployee(Employee employee);
+
+        Employee EditEmployee(Employee employee);
+    }
+}
